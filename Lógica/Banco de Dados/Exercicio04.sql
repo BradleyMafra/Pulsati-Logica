42e5ddda20d9044606b099e0b6b638aac8093fee
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-01-24 15:22:46 -0300

    Aula de banco de dados e resolução de exercicios

---
+++ Lógica/Banco de Dados/Exercicio04.sql
@@ -0,0 +1,2 @@
+-- Faça um select (Query) que traga somente as pessoas onde o nome seja Gael
+SELECT * FROM bradley_usuario WHERE nome LIKE 'Gael';
---
+++ Lógica/Banco de Dados/Exercicio10.sql
@@ -0,0 +1,3 @@
+-- Faça um select (Query) onde traga o email das pessoas que terminem com I
+SELECT email 
+FROM bradley_usuario WHERE email LIKE '%I@%'
