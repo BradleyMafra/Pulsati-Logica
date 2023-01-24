a2e59ba83a393f2a6b8e7d1824cd28650650b9b4
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-01-24 18:01:12 -0300

    Atualizando exercícios

---
+++ Database/Lista02/Exercicio01.sql
@@ -0,0 +1,2 @@
+-- Crie um select que traga todas as pessoas que começem com a letra A ou contenha no meio a letra J ou termine com I
+SELECT * FROM bradley_mafra_funcionarios WHERE nome LIKE '%A' OR nome LIKE '%J%' OR nome LIKE 'I%';
